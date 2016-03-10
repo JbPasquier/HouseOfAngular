853569d61d349effe110ccd93c334e65aaa4c326
Jean-Baptiste <contact@jbpasquier.eu>
2016-03-10 13:20:21 +0100

    git init

---
+++ app.js
@@ -0,0 +1,3 @@
+angular
+    .module('HouseOfAngular',[])
+    .controller('cardController',cardController);
---
+++ js/controllers/cardController.js
@@ -0,0 +1,55 @@
+function cardController($scope, $sce) {
+    $scope.playerOnePlay = '';
+    $scope.playerTwoPlay = '';
+    Object.prototype.shuffle = function() {
+        var j, x, i;
+        for (i = this.length; i; i -= 1) {
+            j = Math.floor(Math.random() * i);
+            x = this[i - 1];
+            this[i - 1] = this[j];
+            this[j] = x;
+        }
+    };
+    $scope.cards = {
+        1: 'as',
+        2: 'deux',
+        3: 'trois',
+        4: 'quatre',
+        5: 'cinq',
+        6: 'six',
+        7: 'sept',
+        8: 'huit',
+        9: 'neuf',
+        10: 'dix',
+        11: 'valet',
+        12: 'dame',
+        13: 'roi'
+    };
+    $scope.colors = [
+        'coeur',
+        'pique',
+        'carreau',
+        'trèfle'
+    ];
+    $scope.cardScope = [];
+    angular.forEach($scope.colors, function(color) {
+        angular.forEach($scope.cards, function(card, index) {
+            $scope.cardScope.push([color, card, index]);
+        });
+    });
+    $scope.players = ['un', 'deux'];
+    $scope.cardByPlayer = Math.floor($scope.cardScope.length / $scope.players.length);
+    $scope.cardScope.shuffle();
+    $scope.playerOneHand = $scope.cardScope.slice(0, $scope.cardByPlayer);
+    $scope.playerTwoHand = $scope.cardScope.slice($scope.cardByPlayer, $scope.cardScope.length);
+    $scope.play = function(player, cardIndex) {
+        if (player === 1) {
+            $scope.playerOnePlay = $scope.playerOneHand[cardIndex][1] + ' de ' + $scope.playerOneHand[cardIndex][0];
+            $scope.playerOneHand.splice(cardIndex,1);
+        } else {
+            $scope.playerTwoPlay = $scope.playerTwoHand[cardIndex][1] + ' de ' + $scope.playerTwoHand[cardIndex][0];
+            $scope.playerTwoHand.splice(cardIndex,1);
+        }
+    };
+
+}
